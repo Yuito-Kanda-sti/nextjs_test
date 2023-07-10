f4d9e04d65e2d8a66cec253347bfc5d09ab2faa9
y-kanda <y-kanda@s.com>
2023-07-10 14:22:41 +0900

    cahnge api_test

--- pages/api/api_test.js
+++ pages/api/api_test.js
@@ -3,6 +3,6 @@ export default async function handler(req, res) {
   const result = await response.json();
   res.status(200).json({host : process.env.BaclEndAPI,results : result})
 
-  console.log(response)
+  //console.log(response)
   //res.status(200).json({status : 333})
 }
