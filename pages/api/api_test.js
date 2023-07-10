049e474c08600fcc5abb98c72aef1df82dd037c1
y-kanda <y-kanda@s.com>
2023-07-10 14:14:53 +0900

    cahnge api_test

--- pages/api/api_test.js
+++ pages/api/api_test.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
   const response = await fetch(process.env.BackEndAPI);
-  //const result = await response.json();
-  //res.status(200).json({host : process.env.BaclEndAPI,results : result})
+  const result = await response.json();
+  res.status(200).json({host : process.env.BaclEndAPI,results : result})
 
   console.log(response)
-  res.status(200).json({status : 333})
+  //res.status(200).json({status : 333})
 }
